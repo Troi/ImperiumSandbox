fe3dd629b3ba8c323056f5876fbaa7b48317d199
vit jurasek <jurasek@internetovy-prodej.cz>
2018-05-08 18:07:03 +0200

    Human extension

--- src/imperium/people/Human.java
+++ src/imperium/people/Human.java
@@ -1,15 +1,19 @@
 package imperium.people;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Human {
     private String name;
     private Human mother;
     private Human father;
     private Family family;
+    private List<Human> children = new ArrayList<>(0);
     private Genome genome = Genome.getRandom();
     private long age = 0;
     private Long diedInAge;
 
-    public Human(String name, Human mother, Human father, Family family) {
+    private Human(String name, Human mother, Human father, Family family) {
         this.name = name;
         this.mother = mother;
         this.father = father;
@@ -19,6 +23,7 @@ public class Human {
     public Human() {
         this.name = Name.getRandomMaleName();
         this.genome = Genome.getRandom();
+        this.family = new Family(this);
     }
 
     public String getName() {
@@ -60,9 +65,43 @@ public class Human {
         return (diedInAge == null);
     }
 
+    public List<Human> getChildren() {
+        return children;
+    }
+
     public Human makeOffspring(Human mother) {
         Human child = new Human(Name.getRandomMaleName(), mother, this, this.family);
+        this.children.add(child);
+        mother.children.add(child);
         child.genome = this.genome.combine(mother.genome);
         return child;
+    }
+
+    public boolean isAncestor(Human human) {
+        boolean directRelationship = (human == this);
+        if (father != null) {
+            directRelationship = directRelationship || father.isAncestor(human);
+        }
+        if (mother != null) {
+            directRelationship = directRelationship || mother.isAncestor(human);
+        }
+        return directRelationship;
+    }
+
+    private int getPreviousGenerationsCount() {
+        int pgf = 0;
+        int pgm = 0;
+        if (father != null) {
+            pgf = 1 + father.getPreviousGenerationsCount();
+        }
+        if (mother != null) {
+            pgm = 1 + mother.getPreviousGenerationsCount();
+        }
+        return Math.max(pgf, pgm);
+    }
+
+    @Override
+    public String toString() {
+        return name + " from " + family.getName() + " (age: " + age + ", generation: " + this.getPreviousGenerationsCount() + ")";
     }
 }
